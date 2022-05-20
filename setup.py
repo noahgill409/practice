a76c205312ef1413db5337b0e2982bf1aa0ccc25
noahgill409 <noahgill409@gmail.com>
2022-05-20 08:19:53 -0500

    update setup files

--- setup.py
+++ setup.py
@@ -1,57 +1,63 @@
+"""setup.py"""
+
 from setuptools import find_packages, setup
+import re
 
 
 def read_requirements(filename: str):
-    with open(filename) as requirements_file:
-        import re
+    """read requirements"""
+    with open(filename, encoding="utf8") as requirements_file:
 
         def fix_url_dependencies(req: str) -> str:
             """Pip and setuptools disagree about how URL dependencies should be handled."""
             m = re.match(
                 r"^(git\+)?(https|ssh)://(git@)?github\.com/([\w-]+)/(?P<name>[\w-]+)\.git", req
             )
+
             if m is None:
                 return req
-            else:
-                return f"{m.group('name')} @ {req}"
+
+            return f"{m.group('name')} @ {req}"
 
         requirements = []
+
         for line in requirements_file:
             line = line.strip()
             if line.startswith("#") or len(line) <= 0:
                 continue
+
             requirements.append(fix_url_dependencies(line))
+
     return requirements
 
 
 # version.py defines the VERSION and VERSION_SHORT variables.
 # We use exec here so we don't import cached_path whilst setting up.
 VERSION = {}  # type: ignore
-with open("my_package/version.py", "r") as version_file:
+with open("my_package/version.py", "r", encoding="utf8") as version_file:
     exec(version_file.read(), VERSION)
 
 setup(
-    name="my-package",
+    name="practice",
     version=VERSION["VERSION"],
-    description="",
-    long_description=open("README.md").read(),
+    description="practice problems for programming interviews 'n such",
+    long_description=open("README.md", encoding="utf8").read(),
     long_description_content_type="text/markdown",
     classifiers=[
         "Intended Audience :: Science/Research",
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     keywords="",
-    url="https://github.com/allenai/python-package-template",
-    author="Allen Institute for Artificial Intelligence",
-    author_email="contact@allenai.org",
+    url="https://github.com/noahgill409/practice",
+    author="noahgill409",
+    author_email="noahgill409@gmail.com",
     license="Apache",
     packages=find_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"],
     ),
-    package_data={"my_package": ["py.typed"]},
+    package_data={"practice": ["py.typed"]},
     install_requires=read_requirements("requirements.txt"),
     extras_require={"dev": read_requirements("dev-requirements.txt")},
     python_requires=">=3.7",
